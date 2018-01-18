4ef395aade69948fd024d531865a2ec2f6217738
brunodutr <brun_dutra@hotmail.com>
2018-01-18 15:10:36 -0200

    Minificar css e js do app

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 const gulp = require('gulp');
 const clean = require('gulp-clean');
+const usemin = require('gulp-usemin');
+const concat = require('gulp-concat');
+const babili = require('gulp-babili');
+const cssmin = require('gulp-cssmin');
 
 gulp.task('copy', ['clean'], function () {
-    gulp.src('src/**/**').pipe(gulp.dest('dist'));
+    gulp.src('src/**/*').pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', function () {
     return gulp.src('dist').pipe(clean());
 });
 
+gulp.task('build', ['copy'], function () {
+    gulp.src('dist/**/*.html')
+        .pipe(usemin({
+            css: [cssmin],
+            js: [babili()]
+        }))
+        .pipe(gulp.dest('dist'));
+});
